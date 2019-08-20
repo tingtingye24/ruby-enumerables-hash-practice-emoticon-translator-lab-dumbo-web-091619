eb600836e239061bd089bf358f6c1d94a18d97c8
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-20 02:42:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,16 @@
 # require modules here
-
+require "yaml"
 def load_library
   # code goes here
+  emotions = YAML.load_file('emoticons.yml')
+  
+  newhash = {
+    japanese_emoticon => {},
+    english_meaning => {}
+  }
+  emotions.each do |key, value|
+    newhash[japanese_emoticon][value[0]] = emotions[] 
+  
 end
 
 def get_japanese_emoticon
